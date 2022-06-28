ed13e1faab9a5eb151ade681dbe06e0f42f27aec
Alexey-Vch-Morozov <102486382+Alexey-Vch-Morozov@users.noreply.github.com>
2022-06-28 20:56:59 +0300

    Add files via upload

---
+++ NewsPortal/portal/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostsSearch, PostCreateView, PostUpdateView, PostDeleteView
+from .views import subscribe
+
+urlpatterns = [
+   path('', PostsList.as_view()),
+   path('<int:pk>', PostDetail.as_view(), name='post'),
+   path('subscribe/<int:pk>', subscribe, name='subscribe'),
+   path('search/', PostsSearch.as_view(), name='search'),
+   path('create/', PostCreateView.as_view(), name='post_create'),  # Ссылка на создание товара
+   path('create/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+   path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+]
---
+++ NewsPortal/portal/views.py
@@ -0,0 +1,130 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.core.mail import EmailMultiAlternatives  # импортируем класс для создание объекта письма с html
+from django.template.loader import render_to_string
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
+from django.core.paginator import Paginator
+from .models import Post, PostCategory, Category
+from .filters import PostFilter
+from .forms import PostForm
+
+
+@receiver(post_save, sender=Post)
+def notify_subscribers(sender, instance, created, **kwargs):
+    if created:
+        # instance_pk = instance.id #Получаем первичный ключ поста
+        # post_category_object = PostCategory.objects.get(post_id=instance_pk) #Получаем объект из ПостКатегори
+        # category_id = post_category_object.category_id #Получаем идентификатор категории
+        # category = Category.objects.get(pk=category_id)
+        category = Category.objects.get(pk=4) #Почему-то не проходит запрос по ПостКатегори - Пост создается,
+        # записывается в базу, но не записывается в таблицу ПостКатегори.
+        emails = [] #Пустой список, чтобы в него записать имейлы
+        for user in category.subscribers.all():
+            if user.email: #Если в поле емайл есть значение, то добавляем в список
+                emails.append(user.email)
+        html_content = render_to_string('new_post_created.html', {'post': instance})
+        print(html_content)
+        msg = EmailMultiAlternatives(subject=instance.header, body=instance.text,
+                                     from_email='alexeyvchmorozov@yandex.ru',
+                                     to=emails) # это то же, что и recipients_list
+        msg.attach_alternative(html_content, "text/html")  # добавляем html
+        #В выводе html_content гиперссылка есть, а в почту не приходит.
+        msg.send()
+
+
+# @receiver(pre_save, sender=Post)
+# def check_3_posts(sender, instance, **kwargs):
+#     enddate = datetime.datetime.now()
+#     startdate = enddate - datetime.timedelta(days=1)
+#     #Набор постов автора за сутки
+#     x = instance.objects.filter(author=instance.author, time_add__range=[startdate, enddate])
+#     if len(x) > 3:
+#         print('Суточное количество постов превышено')
+#     return len(x)
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'time_add'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    form_class = PostForm
+    paginate_by = 10
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)# создаём новую форму, забиваем в неё данные из POST-запроса
+        if form.is_valid():
+            # если пользователь ввёл всё правильно и нигде не ошибся, то сохраняем новый пост
+            # if check_3_posts(sender=Post, instance=form, **kwargs) < 3:
+            form.save()
+            # else:
+            #     print('Количество постов превышено')
+        return super().get(request, *args, **kwargs)
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostsSearch(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'posts'
+    ordering = ['time_add']
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса)
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        return context
+
+class PostCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ('portal.add_post')
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+
+class PostUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    permission_required = ('portal.change_post')   #
+    template_name = 'post_create.html'             #
+    form_class = PostForm                          #
+
+    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся редактировать
+    def get_object(self, *args, **kwargs):                #
+        id = self.kwargs.get('pk')
+        #При обновлении поста отправляем письмо
+        #post = Post.objects.get(pk=id)
+        # html_content = render_to_string('post_changed.html', {'post':post}, {'post_id':id})
+        # msg = EmailMultiAlternatives(subject=post.header, body=post.text, from_email='alexeyvchmorozov@yandex.ru',
+        #                              to=['alexey.vch.morozov@gmail.com']) # это то же, что и recipients_list
+        # msg.attach_alternative(html_content, "text/html")  # добавляем html
+        # msg.send()
+        return Post.objects.get(pk=id)
+
+
+# дженерик для удаления поста
+class PostDeleteView(DeleteView):
+    template_name = 'post_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/'
+
+
+# функция подписки на категории новости
+def subscribe(request, *args, **kwargs):
+    post = Post.objects.get(pk=kwargs['pk'])
+    for category in post.post.all():
+        user = User.objects.get(pk=request.user.id)
+        category.subscribers.add(user)
+    return redirect('/news')
